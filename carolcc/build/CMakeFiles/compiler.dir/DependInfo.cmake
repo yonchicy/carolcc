ec570e8d5c3872445058bf783e6e1c54003f983a
jjcxyjj <69410218+jjcxyjj@users.noreply.github.com>
2022-05-14 16:01:06 +0800

    jyx语法分析部分

---
+++ carolcc/build/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/compiler/carolcc/src/ast.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/ast.cpp.o"
+  "/root/compiler/carolcc/src/frontend/parser.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/frontend/parser.cpp.o"
+  "/root/compiler/carolcc/src/frontend/token.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/frontend/token.cpp.o"
+  "/root/compiler/carolcc/src/helpers/type_helper.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/helpers/type_helper.cpp.o"
+  "/root/compiler/carolcc/src/main.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/main.cpp.o"
+  "/root/compiler/carolcc/src/node.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/node.cpp.o"
+  "/root/compiler/carolcc/src/runtime.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/runtime.cpp.o"
+  "/root/compiler/carolcc/src/syntax_tree.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/syntax_tree.cpp.o"
+  "/root/compiler/carolcc/src/visitor/syntax_tree_builder.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/visitor/syntax_tree_builder.cpp.o"
+  "/root/compiler/carolcc/src/visitor/visitor_base.cpp" "/root/compiler/carolcc/build/CMakeFiles/compiler.dir/src/visitor/visitor_base.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ carolcc/build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "../src/frontend/parser.cpp"
+  "../src/frontend/token.cpp"
+  "CMakeFiles/compiler.dir/src/ast.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/parser.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/token.cpp.o"
+  "CMakeFiles/compiler.dir/src/helpers/type_helper.cpp.o"
+  "CMakeFiles/compiler.dir/src/main.cpp.o"
+  "CMakeFiles/compiler.dir/src/node.cpp.o"
+  "CMakeFiles/compiler.dir/src/runtime.cpp.o"
+  "CMakeFiles/compiler.dir/src/syntax_tree.cpp.o"
+  "CMakeFiles/compiler.dir/src/visitor/syntax_tree_builder.cpp.o"
+  "CMakeFiles/compiler.dir/src/visitor/visitor_base.cpp.o"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
