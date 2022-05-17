6bb7e83f7ab62b5b42ba8d516c4124f71a733662
yonchicy <1079279184@qq.com>
2022-05-17 09:13:45 +0800

    🐳 chore(cmake generated files): rm build files

--- CMakeFiles/compiler.dir/src/helpers/type_helper.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/compiler.dir/src/helpers/type_helper.cpp.o: \
- /home/dongyubo/桌面/Compiler/our_compiler/carolcc/src/helpers/type_helper.cpp \
- /usr/include/stdc-predef.h \
- /home/dongyubo/桌面/Compiler/our_compiler/carolcc/src/helpers/type_helper.h
